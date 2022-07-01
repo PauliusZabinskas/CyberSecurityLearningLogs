a872550625b3693751c5b78f916132985c8c69ab
Paulius <90104135+PauliusZabinskas@users.noreply.github.com>
2022-07-01 15:00:48 +0100

    added iterrations

--- Scraping v-1.py
+++ Scraping v-1.py
@@ -22,7 +22,7 @@ tag = doc.find_all("p")[0]
 #  
 print(tag.find_all("b"))
 
-# v1.2
+# v-1.2
 import string
 from urllib import request
 from bs4 import BeautifulSoup
@@ -34,3 +34,26 @@ with open("index.html", 'r') as html_file:
     content = html_file.read()
     soup = BeautifulSoup(content, 'lxml')
     print(soup.prettify())
+    
+    #v-1.3. iteration is beeeing used to scrape html's 'p' tags
+import string
+from urllib import request
+from bs4 import BeautifulSoup
+
+url = "https://www.amazon.com/ASUS-Graphics-DisplayPort-Axial-tech-2-7-Slot/dp/B0985Z47C8/ref=sr_1_3?crid=15VEGAJCJOS6D&keywords=gpu&qid=1656666149&sprefix=gpu%2Caps%2C315&sr=8-3"
+
+
+with open("index.html", 'r') as html_file:
+    content = html_file.read()
+    soup = BeautifulSoup(content, 'lxml')
+    tags = soup.find_all('p')
+    for course in tags:
+        print(course.text)
+    
+
+
+
+
+    
+    
+    
