af19b4d1302be004c5efb5497935a1f902958378
Paulius <90104135+PauliusZabinskas@users.noreply.github.com>
2022-07-01 09:59:00 +0100

    Update Scraping v-1.py
    added v-1.1

--- Scraping v-1.py
+++ Scraping v-1.py
@@ -5,3 +5,21 @@ with open("index.html","r") as f:
 
 print(doc.prettify())
 
+# more
+from bs4 import BeautifulSoup
+
+with open("index.html","r") as f:
+  doc = BeautifulSoup(f, "html.parser")
+
+tag = doc.find_all("p")[0]
+# doc.find_all("a") = find something
+
+#doc.find("p").text = scrape text only
+
+# tag = doc.find_all("p")[0] && print(tag.find_all("b"))= indexing what i am looking for precisely. 
+# 
+# 
+#  
+print(tag.find_all("b"))
+
+
